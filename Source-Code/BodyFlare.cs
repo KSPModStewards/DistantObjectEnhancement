a55ac1856fdc0d6d666cbcfcb3391f1adf61b017
JonnyOThan <jonnyothan@gmail.com>
2024-10-06 14:53:59 -0400

    Extract BodyFlare and VesselFlare to new files

---
+++ Source-Code/BodyFlare.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace DistantObject
+{
+	// @ 1920x1080, 1 pixel with 60* FoV covers about 2 minutes of arc / 0.03 degrees
+	class BodyFlare : FlareBase
+	{
+		public static double kerbinSMA = -1.0;
+		public static double kerbinRadius;
+
+		// Scale body flare distance to try to ameliorate z-fighting of moons.
+		public static double bodyFlareDistanceScalar = 0.0;
+
+		public static readonly double MinFlareDistance = 739760.0;
+		public static readonly double MaxFlareDistance = 750000.0;
+		public static readonly double FlareDistanceRange = MaxFlareDistance - MinFlareDistance;
+
+		public CelestialBody body;
+		public GameObject bodyMesh;
+		public MeshRenderer meshRenderer;
+		public Renderer scaledRenderer;
+		public Color color;
+		public Vector4 hslColor;
+		public Vector3d cameraToBodyUnitVector;
+		public double distanceFromCamera;
+		public double sizeInDegrees;
+
+		public double relativeRadiusSquared;
+		public double bodyRadiusSquared;
+
+		public void Update(Vector3d camPos, float camFOV)
+		{
+			// Update Body Flare
+			Vector3d targetVectorToSun = FlightGlobals.Bodies[0].position - body.position;
+			Vector3d targetVectorToCam = camPos - body.position;
+
+			double targetSunRelAngle = Vector3d.Angle(targetVectorToSun, targetVectorToCam);
+
+			cameraToBodyUnitVector = -targetVectorToCam.normalized;
+			distanceFromCamera = targetVectorToCam.magnitude;
+
+			double kerbinSMAOverBodyDist = kerbinSMA / targetVectorToSun.magnitude;
+			double luminosity = kerbinSMAOverBodyDist * kerbinSMAOverBodyDist * relativeRadiusSquared;
+			luminosity *= (0.5 + (32400.0 - targetSunRelAngle * targetSunRelAngle) / 64800.0);
+			luminosity = (Math.Log10(luminosity) + 1.5) * (-2.0);
+
+			// We need to clamp this value to remain < 5, since larger values cause a negative resizeVector.
+			// This only appears to happen with some mod-generated worlds, but it's still a good practice
+			// and not terribly expensive.
+			float brightness = Math.Min(4.99f, (float)(luminosity + Math.Log10(distanceFromCamera / kerbinSMA)));
+
+			//position, rotate, and scale mesh
+			targetVectorToCam = ((MinFlareDistance + Math.Min(FlareDistanceRange, distanceFromCamera * bodyFlareDistanceScalar)) * targetVectorToCam.normalized);
+			bodyMesh.transform.position = camPos - targetVectorToCam;
+			bodyMesh.transform.LookAt(camPos);
+
+			float resizeFactor = (-750.0f * (brightness - 5.0f) * (0.7f + .99f * camFOV) / 70.0f) * DistantObjectSettings.DistantFlare.flareSize;
+			bodyMesh.transform.localScale = new Vector3(resizeFactor, resizeFactor, resizeFactor);
+
+			sizeInDegrees = Math.Acos(Math.Sqrt(distanceFromCamera * distanceFromCamera - bodyRadiusSquared) / distanceFromCamera) * Mathf.Rad2Deg;
+
+			// Disable the mesh if the scaledRenderer is enabled and visible.
+			bodyMesh.SetActive(!(scaledRenderer.enabled && scaledRenderer.isVisible));
+		}
+
+		~BodyFlare()
+		{
+			//Debug.Log(Constants.DistantObject + string.Format(" -- BodyFlare {0} Destroy", (body != null) ? body.name : "(null bodyflare?)"));
+		}
+	}
+}
---
+++ Source-Code/VesselFlare.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace DistantObject
+{
+	class VesselFlare : FlareBase
+	{
+		public Vessel referenceShip;
+		public GameObject flareMesh;
+		public MeshRenderer meshRenderer;
+		public float luminosity;
+		public float brightness;
+
+		public void Update(Vector3d camPos, float camFOV)
+		{
+			try
+			{
+				Vector3d targetVectorToCam = camPos - referenceShip.transform.position;
+				float targetDist = (float)Vector3d.Distance(referenceShip.transform.position, camPos);
+				bool activeSelf = flareMesh.activeSelf;
+				if (targetDist > 750000.0f && activeSelf)
+				{
+					flareMesh.SetActive(false);
+					activeSelf = false;
+				}
+				else if (targetDist < 750000.0f && !activeSelf)
+				{
+					flareMesh.SetActive(true);
+					activeSelf = true;
+				}
+
+				if (activeSelf)
+				{
+					brightness = Mathf.Log10(luminosity) * (1.0f - Mathf.Pow(targetDist / 750000.0f, 1.25f));
+
+					flareMesh.transform.position = camPos - targetDist * targetVectorToCam.normalized;
+					flareMesh.transform.LookAt(camPos);
+					float resizeFactor = (0.002f * targetDist * brightness * (0.7f + .99f * camFOV) / 70.0f) * DistantObjectSettings.DistantFlare.flareSize;
+
+					flareMesh.transform.localScale = new Vector3(resizeFactor, resizeFactor, resizeFactor);
+					//Debug.Log(string.Format("Resizing vessel flare {0} to {1} - brightness {2}, luminosity {3}", referenceShip.vesselName, resizeFactor, brightness, luminosity));
+				}
+			}
+			catch
+			{
+				// If anything went whack, let's disable ourselves
+				flareMesh.SetActive(false);
+				referenceShip = null;
+			}
+		}
+
+		~VesselFlare()
+		{
+			// Why is this never called?
+			//Debug.Log(Constants.DistantObject + string.Format(" -- VesselFlare {0} Destroy", (referenceShip != null) ? referenceShip.vesselName : "(null vessel?)"));
+		}
+	}
+}
